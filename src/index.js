b207c9a115a591ab99c8afcaec3c894e1e9f61f0
kili196 <kilian.baier16@gmail.com>
2024-08-08 14:57:47 +0200

    switching between sites

--- src/index.js
+++ src/index.js
@@ -4,6 +4,38 @@ import {returnMenu}  from "./menu"
 
 
 let mainContainer = document.getElementById("content");
-mainContainer.appendChild(returnMenu())
+let navBar = document.getElementsByClassName("navbar")[0];
+
+
+Array.from(navBar.getElementsByTagName("button")).forEach((element) => {
+    
+    switch(element.innerHTML) {
+        case "Home":
+            element.addEventListener("click", () => {
+            mainContainer.firstChild.remove();
+            console.log("hii")
+            })
+            
+        
+            break;
+        case "Menu":
+         
+        element.addEventListener("click", () => {
+            mainContainer.firstChild.remove();
+            mainContainer.appendChild(returnMenu())
+            console.log("hii")
+            })
+          
+            break;
+        case "Get your Reservation":
+            element.addEventListener("click", () => {
+                mainContainer.firstChild.remove();
+                console.log("hii")
+                })
+            
+            break;
+    }
+})
+
 
 
