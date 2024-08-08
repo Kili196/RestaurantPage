b274e3f559f9bb1f12b189f114070e3695b5c304
kili196 <kilian.baier16@gmail.com>
2024-08-08 15:06:43 +0200

    showing menu working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./styles.css"
-import {returnMenu}  from "./menu"
+import { returnMenu } from "./menu/menu"
 
 
 
@@ -7,35 +7,41 @@ let mainContainer = document.getElementById("content");
 let navBar = document.getElementsByClassName("navbar")[0];
 
 
+mainContainer.appendChild(returnMenu());
+
+
 Array.from(navBar.getElementsByTagName("button")).forEach((element) => {
-    
-    switch(element.innerHTML) {
+
+    switch (element.innerHTML) {
         case "Home":
             element.addEventListener("click", () => {
-            mainContainer.firstChild.remove();
-            console.log("hii")
+                mainContainer.firstChild.remove();
             })
-            
-        
             break;
         case "Menu":
-         
-        element.addEventListener("click", () => {
-            mainContainer.firstChild.remove();
-            mainContainer.appendChild(returnMenu())
-            console.log("hii")
+            element.addEventListener("click", () => {
+                mainContainer.firstChild.remove();
+                mainContainer.appendChild(returnMenu())
+
+                console.log("hii")
             })
-          
+
             break;
         case "Get your Reservation":
             element.addEventListener("click", () => {
                 mainContainer.firstChild.remove();
                 console.log("hii")
-                })
-            
+            })
+
             break;
     }
 })
+
+function removeAllChilds(node) {
+    while (node.firstChild) {
+        node.firstChild.remove;
+    }
+}
 
 
 
